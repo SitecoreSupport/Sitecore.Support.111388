06e14479e8771f03d060644cf49fd793378ec6be
Alexey Tupkalo <altu@sitecore.net>
2017-09-28 11:01:10 +0300

    Patch 111388 initial commit

---
+++ src/Sitecore.Support.111388/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.111388")]
+[assembly: AssemblyProduct("Sitecore.Support.111388")]
+[assembly: ComVisible(false)]
